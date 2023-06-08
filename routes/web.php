d0c4e2fcb1d846476308d573dda7e7f8a8ca23fc
Azziadati <122371599+azziadati@users.noreply.github.com>
2023-06-08 12:04:31 +0700

    TUGAS 24

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,33 @@ Route::get('/', function () {
     return view('landing');
 });
 
+//Dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+//Category
 Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
+Route::get('/category/edit/{id}', [CategoryController::class, 'edit'])->name('category.edit');
+Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
+Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+
+//Product
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/role', [RoleController::class, 'index'])->name('role.index');
+
+    Route::middleware('role:Admin|Staff')->group(function() {
+        Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+        Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+        Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+        Route::put('/product/{id}', [ProductController::class, 'update'])->name('product.update');
+        Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+    });
+
+//User
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
+//Role
+Route::get('/role', [RoleController::class, 'index'])->name('role.index');
+
+
 
